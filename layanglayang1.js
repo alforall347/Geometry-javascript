ed253cc7823743533a1a79e6f161ccd40ffbfc8c
RafiKurniAwan347 <warscandyyt@gmail.com>
2026-05-26 16:14:07 +0700

    nyicil

---
+++ layanglayang1.js
@@ -0,0 +1,11 @@
+let d1 = 12;
+let d2 = 8;
+let sisi1 = 5;
+let sisi2 = 7;
+
+let luas = (d1 * d2) / 2;
+let keliling = 2 * (sisi1 + sisi2);
+
+console.log("Layang-Layang");
+console.log("Luas = " + luas);
+console.log("Keliling = " + keliling);
